1a8d19a4fccabe68fbf432eec609839d863e08a7
hrsk <hrsk.yegor@gmail.com>
2023-10-29 15:13:41 +0300

    trim method, autofocus,added error output if input is empty at editable span component

--- src/editable_span/EditableSpan.tsx
+++ src/editable_span/EditableSpan.tsx
@@ -8,6 +8,7 @@ export const EditableSpan = (props: PropsType) => {
 
     const [edit, setEdit] = useState(false);
     const [value, setValue] = useState(props.value);
+    const [error, setError] = useState('');
 
     const onChangeHandler = (event: ChangeEvent<HTMLInputElement>) => {
         setValue(event.currentTarget.value)
@@ -17,23 +18,44 @@ export const EditableSpan = (props: PropsType) => {
     }
 
     const viewModeHandler = () => {
-        props.callback(value)
-        setEdit(false)
+        if (value.trim() !== '') {
+            props.callback(value)
+            setEdit(false)
+            setError('')
+        } else {
+            setError('Invalid input value!')
+        }
     }
 
     const onKeyPressHandler = (event: KeyboardEvent<HTMLInputElement>) => {
         if (event.key === 'Enter' && event.metaKey) {
-            viewModeHandler()
+            if (value.trim() !== '') {
+                viewModeHandler()
+                setError('')
+            } else {
+                setError('Invalid input value!')
+            }
         }
     }
 
     return (
-        edit
-            ? <input value={value}
-                     onChange={onChangeHandler}
-                     onBlur={viewModeHandler}
-                     onKeyDown={onKeyPressHandler}
-            />
-            : <span onDoubleClick={editModeHandler}>{props.value}</span>
+        <> {
+            edit
+                ? <input style={error ? {borderColor: 'crimson'} : {borderColor: 'black'}}
+                         value={value}
+                         onChange={onChangeHandler}
+                         onBlur={viewModeHandler}
+                         onKeyDown={onKeyPressHandler}
+                         autoFocus
+                />
+                : <span onDoubleClick={editModeHandler}>{props.value}</span>
+        }
+            {error
+                ? <div style={{color: 'crimson'}}>
+                    {error}
+                </div>
+                : ''
+            }
+        </>
     );
 };
