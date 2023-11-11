b44267a16dd4115528590fff876883ef5727998d
hrsk <hrsk.yegor@gmail.com>
2023-11-11 19:01:10 +0300

    reducer cases, action creators, reducers case-tests

--- src/state/reducers/tasks-reducer.test.ts
+++ src/state/reducers/tasks-reducer.test.ts
@@ -1,4 +1,4 @@
-import {removeTaskAC, tasksReducer} from './tasks-reducer'
+import {addTaskAC, changeTaskStatusAC, changeTaskTitleAC, removeTaskAC, tasksReducer} from './tasks-reducer'
 import {TasksType} from "../../index";
 
 test('correct task should be deleted from correct array', () => {
@@ -30,4 +30,79 @@ test('correct task should be deleted from correct array', () => {
             {id: '3', title: 'tea', isDone: false},
         ],
     })
+});
+
+test('correct task should be added to correct array', () => {
+    const startState: TasksType = {
+        'todolistId1': [
+            {id: '1', title: 'CSS', isDone: false},
+            {id: '2', title: 'JS', isDone: true},
+            {id: '3', title: 'React', isDone: false},
+        ],
+        'todolistId2': [
+            {id: '1', title: 'bread', isDone: false},
+            {id: '2', title: 'milk', isDone: true},
+            {id: '3', title: 'tea', isDone: false},
+        ],
+    }
+
+    const action = addTaskAC('todolistId2', 'juice');
+
+    const endState = tasksReducer(startState, action);
+
+    expect(endState['todolistId1'].length).toBe(3)
+    expect(endState['todolistId2'].length).toBe(4)
+    expect(endState['todolistId2'][0].id).toBeDefined()
+    expect(endState['todolistId2'][0].title).toBe('juice')
+    expect(endState['todolistId2'][0].isDone).toBe(false)
+});
+
+test('status of specified task should be changed', () => {
+    const startState: TasksType = {
+        'todolistId1': [
+            {id: '1', title: 'CSS', isDone: false},
+            {id: '2', title: 'JS', isDone: true},
+            {id: '3', title: 'React', isDone: false},
+        ],
+        'todolistId2': [
+            {id: '1', title: 'bread', isDone: false},
+            {id: '2', title: 'milk', isDone: true},
+            {id: '3', title: 'tea', isDone: false},
+        ],
+    }
+
+    const action = changeTaskStatusAC('todolistId2', '2', false)
+
+    const endState = tasksReducer(startState, action)
+
+    expect(endState['todolistId1'].length).toBe(3)
+    expect(endState['todolistId2'].length).toBe(3)
+    expect(endState['todolistId2'][1].id).toBe('2')
+    expect(endState['todolistId2'][1].title).toBe('milk')
+    expect(endState['todolistId2'][1].isDone).toBe(false)
+});
+
+test('correct task title should be changed', () => {
+    const startState: TasksType = {
+        'todolistId1': [
+            {id: '1', title: 'CSS', isDone: false},
+            {id: '2', title: 'JS', isDone: true},
+            {id: '3', title: 'React', isDone: false},
+        ],
+        'todolistId2': [
+            {id: '1', title: 'bread', isDone: false},
+            {id: '2', title: 'milk', isDone: true},
+            {id: '3', title: 'tea', isDone: false},
+        ],
+    }
+
+    const action = changeTaskTitleAC('todolistId2', '2', 'false')
+
+    const endState = tasksReducer(startState, action)
+
+    expect(endState['todolistId1'].length).toBe(3)
+    expect(endState['todolistId2'].length).toBe(3)
+    expect(endState['todolistId2'][1].id).toBe('2')
+    expect(endState['todolistId2'][1].title).toBe('false')
+    expect(endState['todolistId2'][1].isDone).toBe(true)
 });
--- src/state/reducers/tasks-reducer.ts
+++ src/state/reducers/tasks-reducer.ts
@@ -1,25 +1,77 @@
 import {TasksType, TaskType} from "../../index";
+import {v1} from "uuid";
 
 export const tasksReducer = (state: TasksType, action: ActionsType): TasksType => {
     switch (action.type) {
         case 'REMOVE_TASK':
             return {
-                ...state, [action.todolistId]: state[action.todolistId].filter((task: TaskType) => task.id !== action.taskId)
+                ...state,
+                [action.todolistId]: state[action.todolistId].filter((task: TaskType) => task.id !== action.taskId)
             }
+        case 'ADD_TASK':
+            return {
+                ...state,
+                [action.todolistId]: [{id: v1(), title: action.value, isDone: false}, ...state[action.todolistId]]
 
+            }
+        case 'CHANGE_TASK_STATUS':
+            return {
+                ...state,
+                [action.todolistId]: state[action.todolistId].map(task => task.id === action.taskId ? {
+                    ...task,
+                    isDone: action.isDone
+                } : task)
+            }
+        case 'CHANGE_TASK_TITLE':
+            return {
+                ...state,
+                [action.todolistId]: state[action.todolistId].map(task => task.id === action.taskId ? {
+                    ...task,
+                    title: action.value
+                } : task)
+            }
         default:
-            throw new Error('I don\'t understand this type')
+            return state
     }
 }
 
 type RemoveTaskActionType = ReturnType<typeof removeTaskAC>
+type AddTaskActionType = ReturnType<typeof addTaskAC>
+type ChangeTaskStatusActionType = ReturnType<typeof changeTaskStatusAC>
+type ChangeTaskTitleActionType = ReturnType<typeof changeTaskTitleAC>
 
 type ActionsType = RemoveTaskActionType
+    | AddTaskActionType
+    | ChangeTaskStatusActionType
+    | ChangeTaskTitleActionType
 
 export const removeTaskAC = (todolistId: string, taskId: string) => {
     return {
         type: 'REMOVE_TASK',
         todolistId,
         taskId,
+    } as const
+}
+export const addTaskAC = (todolistId: string, value: string) => {
+    return {
+        type: 'ADD_TASK',
+        todolistId,
+        value,
+    } as const
+}
+export const changeTaskStatusAC = (todolistId: string, taskId: string, isDone: boolean) => {
+    return {
+        type: 'CHANGE_TASK_STATUS',
+        todolistId,
+        taskId,
+        isDone,
+    } as const
+}
+export const changeTaskTitleAC = (todolistId: string, taskId: string, value: string) => {
+    return {
+        type: 'CHANGE_TASK_TITLE',
+        todolistId,
+        taskId,
+        value,
     } as const
 }
