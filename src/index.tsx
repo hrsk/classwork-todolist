243fd0a28265a9a741d4abe759ede74ad970d47f
hrsk <hrsk.yegor@gmail.com>
2023-11-13 19:35:40 +0300

    useReducer, fix action creators names on todolist reducer test

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import {App} from './App';
+// import {App} from './App';
 import reportWebVitals from './reportWebVitals';
+import {AppWithReducer} from "./AppWithReducer";
 
 export type TaskType = {
     id: string
@@ -24,7 +25,7 @@ const root = ReactDOM.createRoot(
     document.getElementById('root') as HTMLElement
 );
 root.render(
-    <App/>,
+    <AppWithReducer/>,
 );
 
 // If you want to start measuring performance in your app, pass a function
