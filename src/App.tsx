9c414b931f8ed7149f01914c0d150526209a7979
hrsk <hrsk.yegor@gmail.com>
2024-02-02 22:44:22 +0300

    clear all, new project

--- src/App.tsx
+++ src/App.tsx
@@ -1,106 +1,25 @@
-import { Container, Grid } from "@mui/material";
-import { memo, useCallback } from "react";
-import { useDispatch } from "react-redux";
-import "./App.css";
-import { ListsComponent } from "./Lists";
-import DrawerAppBar from "./ResponsiveAppbar";
-import { FilterValuesType } from "./index";
-import { InputWithButton } from "./input_with_button/InputWithButton";
-import {
-    addTaskAC,
-    changeTaskStatusAC,
-    changeTaskTitleAC,
-    removeTaskAC,
-} from "./state/reducers/tasks-reducer";
-import {
-    addTodolistAC,
-    changeTodolistFilterAC,
-    changeTodolistTitleAC,
-    removeTodolistAC,
-} from "./state/reducers/todolist-reducer";
+import './App.css';
 
-export const App = memo(() => {
-    const dispatch = useDispatch();
-
-    const removeTask = useCallback(
-        (todolistId: string, taskId: string) => {
-            dispatch(removeTaskAC(todolistId, taskId));
-        },
-        [dispatch]
-    );
-
-    const changeTaskStatus = useCallback(
-        (todolistId: string, taskId: string, isDone: boolean) => {
-            dispatch(changeTaskStatusAC(todolistId, taskId, isDone));
-        },
-        [dispatch]
-    );
-
-    const changeTaskFilter = useCallback(
-        (todolistId: string, filter: FilterValuesType) => {
-            dispatch(changeTodolistFilterAC(todolistId, filter));
-        },
-        [dispatch]
-    );
-
-    const addTask = useCallback(
-        (todolistId: string, value: string) => {
-            dispatch(addTaskAC(todolistId, value));
-        },
-        [dispatch]
-    );
-
-    const addTodolist = useCallback(
-        (value: string) => {
-            dispatch(addTodolistAC(value));
-        },
-        [dispatch]
-    );
-
-    const removeTodolist = useCallback(
-        (todolistId: string) => {
-            dispatch(removeTodolistAC(todolistId));
-        },
-        [dispatch]
-    );
-
-    const changeTaskTitle = useCallback(
-        (todolistId: string, taskId: string, title: string) => {
-            dispatch(changeTaskTitleAC(todolistId, taskId, title));
-        },
-        [dispatch]
-    );
-
-    const changeTodolistTitle = useCallback(
-        (todolistId: string, title: string) => {
-            dispatch(changeTodolistTitleAC(todolistId, title));
-        },
-        [dispatch]
-    );
-
+export const App = () => {
     return (
-        <>
-            <DrawerAppBar />
-            <div className="App">
-                <Container fixed>
-                    <Grid container>
-                        <InputWithButton
-                            callbackHandler={addTodolist}
-                            name={"+"}
-                        />
-                    </Grid>
-                    <ListsComponent
-                        removeTask={removeTask}
-                        changeTaskStatus={changeTaskStatus}
-                        changeFilter={changeTaskFilter}
-                        addTask={addTask}
-                        removeTodolist={removeTodolist}
-                        changeTaskTitle={changeTaskTitle}
-                        changeTodolistTitle={changeTodolistTitle}
-                        addTodolist={addTodolist}
-                    />
-                </Container>
+        <div className="App">
+            <div>
+                <h3>What to learn</h3>
+                <div>
+                    <input />
+                    <button>+</button>
+                </div>
+                <ul>
+                    <li><input type="checkbox" checked={true} /><span>HTML&CSS</span></li>
+                    <li><input type="checkbox" checked={true} /><span>JS</span></li>
+                    <li><input type="checkbox" checked={false} /><span>React</span></li>
+                </ul>
+                <div>
+                    <button>All</button>
+                    <button>Active</button>
+                    <button>Completed</button>
+                </div>
             </div>
-        </>
+        </div>
     );
-});
+}
