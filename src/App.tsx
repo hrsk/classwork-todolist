448a7a8f5314bc18bcf1e9f2e11545b034afa342
hrsk <hrsk.yegor@gmail.com>
2024-02-03 18:26:25 +0300

    use props, rendering tasks using index

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,26 @@
 import './App.css';
+import { Todolist } from './Todolist';
 
 export const App = () => {
+
+    const tasks1 = [
+        { id: 1, title: 'HTML & CSS', isDone: true },
+        { id: 2, title: 'JS', isDone: false },
+        { id: 3, title: 'React', isDone: false },
+    ];
+
+    const tasks2 = [
+        { id: 1, title: 'Bread', isDone: true },
+        { id: 2, title: 'Milk', isDone: false },
+        { id: 3, title: 'Butter', isDone: false },
+        { id: 4, title: 'Coca-cola', isDone: false },
+    ];
+
     return (
         <div className="App">
-            <div>
+            <Todolist tasks={tasks1} title={'What to learn'} />
+            <Todolist tasks={tasks2} title={'What to buy'} />
+            {/* <div>
                 <h3>What to learn</h3>
                 <div>
                     <input />
@@ -19,7 +36,7 @@ export const App = () => {
                     <button>Active</button>
                     <button>Completed</button>
                 </div>
-            </div>
+            </div> */}
         </div>
     );
 }
