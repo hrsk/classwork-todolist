74ed7a3face0367c192fd9d5101ae4d1176225b4
hrsk <hrsk.yegor@gmail.com>
2024-02-08 18:59:16 +0300

    action type, action creator

--- src/todolists-reducer.ts
+++ src/todolists-reducer.ts
@@ -1,8 +1,22 @@
 import { TodolistType } from "./types"
 
-export const todolistsReducer = (state: Array<TodolistType>, action: any) => {
+export const todolistsReducer = (state: Array<TodolistType>, action: ActionsType) => {
     switch (action.type) {
         case 'REMOVE_TODOLIST': return state.filter(todolist => todolist.id !== action.todolistId)
         default: return state
+    }
+}
+
+export type RemoveTodolistActionType = {
+    type: 'REMOVE_TODOLIST'
+    todolistId: string
+}
+
+type ActionsType = RemoveTodolistActionType;
+
+export const removeTodolistAC = (todolistId: string): RemoveTodolistActionType => {
+    return {
+        type: 'REMOVE_TODOLIST',
+        todolistId,
     }
 }
